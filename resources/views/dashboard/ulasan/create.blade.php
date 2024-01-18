cc24176161dc33399ab78d5df1e88d3dc9d87245
pratiartnw <pratiaretnowidiawati16@gmail.com>
2024-01-18 09:48:26 +0700

    update view ulasan

--- resources/views/dashboard/ulasan/create.blade.php
+++ resources/views/dashboard/ulasan/create.blade.php
@@ -61,11 +61,11 @@
                     <div class="form-group mb-3">
                         <label for="rating">Rating</label>
                         <div class="rating">
-                            <input type="radio" id="star1" name="rating" value="1"><label for="star1" title="1 star"></label>
-                            <input type="radio" id="star2" name="rating" value="2"><label for="star2" title="2 stars"></label>
-                            <input type="radio" id="star3" name="rating" value="3"><label for="star3" title="3 stars"></label>
+                            <input type="radio" id="star5" name="rating" value="5"><label for="star5" title="5 star"></label>
                             <input type="radio" id="star4" name="rating" value="4"><label for="star4" title="4 stars"></label>
-                            <input type="radio" id="star5" name="rating" value="5"><label for="star5" title="5 stars"></label>
+                            <input type="radio" id="star3" name="rating" value="3"><label for="star3" title="3 stars"></label>
+                            <input type="radio" id="star2" name="rating" value="2"><label for="star2" title="2 stars"></label>
+                            <input type="radio" id="star1" name="rating" value="1"><label for="star5" title="1 stars"></label>
                         </div>
                         @error('rating')
                             <div class="d-block invalid-feedback">{{ $message }}</div>
